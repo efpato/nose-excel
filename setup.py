aff4cd158ffac3d6043cd455e1a5ed4c1a500aa9
Sergey Demenok <sergey.demenok@gmail.com>
2015-02-06 21:27:04 +0300

    init repository

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='nose-excel',
+    version='1.0.0',
+    url='https://github.com/efpato/nose-excel',
+    author='Sergey Demenok',
+    author_email='sergey.demenok@gmail.com',
+    description='Nose plugin for excel report',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+    ],
+    py_modules=['excel'],
+    entry_points={
+        'nose.plugins.0.10': [
+            'excel = excel:Excel'
+        ]
+    }
+)
