09c14af7c168603c5e3128b71eb0ea162f369e14
Sergey Demenok <sergey.demenok@gmail.com>
2015-02-20 11:07:06 +0300

    send mail

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='nose-excel',
-    version='1.0.2',
+    version='1.2.0',
     url='https://github.com/efpato/nose-excel',
     author='Sergey Demenok',
     author_email='sergey.demenok@gmail.com',
